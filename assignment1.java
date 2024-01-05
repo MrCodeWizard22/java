9d10d0f1d5f1d2a551087e5bcc34536422c5289b
Piyush Varshney <piyushvarshney54@gmail.com>
2024-01-05 19:11:05 +0530

    basics

---
+++ assignment1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class assignment1 {
+    public static void main(String []args){
+        
+        Scanner sc=new Scanner(System.in);
+        // In a program,input3numbers:A,B and C.You have to output the average of these 3 numbers
+        
+        // float a=sc.nextFloat();
+        // float b=sc.nextFloat();
+        // float c=sc.nextFloat();
+
+        // float avr=(a+b+c)/3f;
+
+        // System.out.println(avr);
+
+
+        // find the area of square
+
+        // float side=sc.nextFloat();
+        // float area=side*side;
+        // System.out.println(area);
+
+
+        // byte b=4;
+        // char c='a';
+        // short s=4;
+        // int i=1000;
+        // float f=3.14f;
+        // double d=99.9954;
+
+        // double result=(f*b)+(i%c)-(d*s);
+
+        // System.out.println (result);
+
+        // int $=24;
+        // System.out.println($);
+
+        
+    }    
+}
