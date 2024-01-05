ee5b44f3903bc783bd8d899cce07558190c6e0d6
Piyush Varshney <piyushvarshney54@gmail.com>
2024-01-05 18:54:03 +0530

    first commit

---
+++ basics.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+public class basics{
+    public static void main(String []args){
+        // int a=20;
+        // int b=10;
+        // int sum=a+b;
+        // System.out.println(sum);
+
+        // input in java
+
+        Scanner sc=new Scanner(System.in);
+
+
+        // String input=sc.next();
+        // System.out .print (input);
+
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int sum=a+b;
+        // System.out.println(sum);
+
+        // int product =a*b;
+        // System.out.println(product);
+
+        // area of circle 
+
+        // float rad=sc.nextFloat();
+        // float area=3.14f *rad *rad;                     //if we write 3.14 without defining the type then it will automatically take it as double 
+        //                                                 //to resolve this we write  f after 3.14
+
+        // System.out.println(area);
+
+
+
+
+        
+        // type conversion 
+
+        // --the conversion will be on desired type if we try to convert the int to bool then it is not possible 
+        // --again the size of destination type should be greater than source type 
+
+        // if we try to convert the int to long then it is valid and long to int is invalid 
+
+        // int a=10;
+        // long b=a;
+        // System.out.println(b);// it is correct but
+
+        // long a=10;
+        // int b=a;
+        // System.out.println(b);   //will show error
+
+        // byte->short ->int ->float ->long ->double
+
+
+
+
+
+        // type casting 
+
+        // type conversionby user itself 
+
+        // float f=12.75f;
+        // int a=(int )f;
+        // System.out.println(a);
+
+
+
+
+        // type promotion
+
+        // byte short char automatically evaluated as int in java 
+
+    }
+}
