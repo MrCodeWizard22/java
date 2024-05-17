42202a6a7d7d1f6a57c626fe50ad0a0ef50e0a0d
Piyush Varshney <piyushvarshney54@gmail.com>
2024-05-17 22:31:50 +0530

    new files added

---
+++ BinaryTrees2.java
@@ -0,0 +1,84 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTrees2 {
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    static class Trees {
+        static int i = -1;
+
+        public static Node create(int arr[]) {
+            i++;
+            if (arr[i] == -1) {
+                return null;
+            }
+            Node newnode = new Node(arr[i]);
+            newnode.left = create(arr);
+            newnode.right = create(arr);
+
+            return newnode;
+        }
+
+        public static void levelOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while (!q.isEmpty()) {
+                Node curr = q.remove();
+                if (curr == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(curr.val);
+                    if (curr.left != null) {
+                        q.add(curr.left);
+                    }
+                    if (curr.right != null) {
+                        q.add(curr.right);
+                    }
+                }
+            }
+        }
+
+        public static void kLevel(Node root, int level, int k) {
+            if (root == null) {
+                return;
+
+            }
+            if (level == k) {
+                System.out.print(root.val + "  ");
+                return;
+            }
+            kLevel(root.left, level + 1, k);
+            kLevel(root.right, level + 1, k);
+
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1 };
+        Trees t = new Trees();
+        Node root = t.create(arr);
+        // t.levelOrder(root);
+
+        t.kLevel(root, 1, 3);
+    }
+}
