9e067c07a0e9c1cf30d62d6de7638ec505db8a66
Piyush Varshney <piyushvarshney54@gmail.com>
2024-06-09 12:08:38 +0530

    modified

--- BST1.java
+++ BST1.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BST1 {
     static class Node {
         int val;
@@ -37,6 +40,9 @@ public class BST1 {
 
     // search function
     public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
         if (root.val == key) {
             return true;
         } else if (root.val < key) {
@@ -47,13 +53,57 @@ public class BST1 {
         return false;
     }
 
+    // utility function for printPaths
+    public static void print(List<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + "--> ");
+        }
+        System.out.println();
+    }
+
+    // printing the paths (root ->leaf)
+    public static void printPaths(Node root, List<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        if (root.left == null && root.right == null) {
+            print(path);
+        }
+        printPaths(root.left, path);
+        printPaths(root.right, path);
+        path.remove(path.size() - 1);
+    }
+
+    // mirror of the bst
+    public static Node mirror(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node l = mirror(root.left);
+        Node r = mirror(root.right);
+
+        root.left = r;
+        root.right = l;
+        return root;
+    }
+
     public static void main(String args[]) {
-        int values[] = { 5, 1, 3, 4, 2, 7 };
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
         inorder(root);
-        System.out.println(search(root, 7));
+        try {
+            System.out.println(search(root, 7));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        printPaths(root, new ArrayList<>());
+
+        System.out.println("after mirror");
+        root = mirror(root);
+        inorder(root);
     }
 }
