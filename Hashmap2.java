203f346cbc6fb2808b70e2e4df9e9f9489caae73
Piyush Varshney <piyushvarshney54@gmail.com>
2024-08-25 21:58:25 +0530

    new files added

---
+++ Hashmap2.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Hashmap2 {
+    public static void main(String... args) {
+        int arr[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (Integer key : hm.keySet()) {
+            if (hm.get(key) > arr.length / 3) {
+                System.out.println(key);
+            }
+        }
+    }
+}
